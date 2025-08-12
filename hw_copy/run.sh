79efb903ee4230c7c257502c62ddddeede45d7cc
王博阳 <wby_2006@petalmail.com>
2025-08-12 18:05:28 +0800

    Update run.sh

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,9 @@
-# Add your commands here
+#!/bin/bash
+docker build -t hw_copy_image -f ./Dockerfile .
+echo "请选择要运行的程序（add/mul）："
+read program
+if [ "$program" != "add" ] && [ "$program" != "mul" ]; then
+    echo "错误：请输入 add 或 mul"
+    exit 1
+fi
+docker run hw_copy_image ./$program
